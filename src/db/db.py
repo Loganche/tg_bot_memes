29bfe59cfb92794507702907bb408be3659b62c5
Alexander Loginov <lxndrloginov@gmail.com>
2024-10-21 19:23:31 +0300

    Added admin crawling and reacting

--- src/db/db.py
+++ src/db/db.py
@@ -17,6 +17,10 @@ class Sqlite3:
             '''CREATE TABLE IF NOT EXISTS channels
                             (channel_id INTEGER PRIMARY KEY, message_id INTEGER NOT NULL)'''
         )
+        self.cursor.execute(
+            '''CREATE TABLE IF NOT EXISTS admins
+                            (user_id INTEGER PRIMARY KEY, message_id INTEGER NOT NULL)'''
+        )
         self.connection.commit()
         res = self.cursor.execute('SELECT name FROM sqlite_master')
         logging.info(f'Got tables: {res.fetchall()}')
@@ -37,9 +41,23 @@ class Sqlite3:
             message_id = -1
         return message_id
 
+    def get_admin_offset(self, user_id):
+        res = self.cursor.execute('SELECT message_id FROM admins WHERE user_id=?', [user_id])
+        self.connection.commit()
+        try:
+            message_id = res.fetchone()[0]
+        except Exception:
+            message_id = -1
+        return message_id
+
     def insert_new_channels(self, values):
         logging.info(f'Adding {len(values)} new channels from file')
         self.cursor.executemany('INSERT INTO channels VALUES (?, ?)', values)
+        self.connection.commit()
+
+    def insert_new_admins(self, values):
+        logging.info(f'Adding {len(values)} new admins from file')
+        self.cursor.executemany('INSERT INTO admins VALUES (?, ?)', values)
         self.connection.commit()
 
     def update_channel_offset(self, channel_id, message_id):
@@ -47,4 +65,9 @@ class Sqlite3:
         self.cursor.execute(
             'UPDATE channels SET message_id=? WHERE channel_id=?', [message_id, channel_id]
         )
+        self.connection.commit()
+
+    def update_admin_offset(self, user_id, message_id):
+        logging.info(f'Updating admin {user_id} offset with message {message_id}')
+        self.cursor.execute('UPDATE admins SET message_id=? WHERE user_id=?', [message_id, user_id])
         self.connection.commit()
--- src/tglib/tg_client.py
+++ src/tglib/tg_client.py
@@ -3,6 +3,7 @@
 import json
 import logging
 import datetime
+from asyncio import sleep
 
 from telethon import TelegramClient
 
@@ -14,8 +15,10 @@ class TGClientError(Exception):
 class TGClient(TelegramClient):
     INITIAL_MSG_OFFSET = 5
     MESSAGE_SCHEDULE = 5
-    TG_BOT_NAME = 'MBerushiBot'  # receiver
+    TG_BOT_NAME = 'bapbab'
     TG_ADMIN_NAME = 'loganche'
+    TG_CHANNEL_NAME = 'tstbapbab'
+    REACTIONS_APPROVED_AMOUNT = 1
 
     def __init__(
         self,
@@ -53,7 +56,7 @@ class TGClient(TelegramClient):
 
         for channel in self.channels:
             tg_channel = await self.get_input_entity(channel['name'])
-            if channels_db.get(tg_channel.channel_id, -1) == -1:
+            if channels_db.get(tg_channel.channel_id) is None:
                 to_add.append(tg_channel.channel_id)
 
         if to_add:
@@ -66,35 +69,26 @@ class TGClient(TelegramClient):
             await self.crawl_channel_messages(channel)
 
     async def crawl_channel_messages(self, channel):
+        """
+        Get tg channel name and offset
+        Receive all messages from offset to now
+        Iterate over messages and send them to admin
+        Find max message and update offset
+        """
         tg_channel = await self.get_input_entity(channel['name'])
         message_id = self.db.get_channel_offset(tg_channel.channel_id)
 
-        if message_id == -1:
-            logging.error(f'Channel {tg_channel.channel_id} not initialized, skipping...')
-            return
-        elif message_id == 0:
-            # crawling only last messages - OFFSET for new channels with 0 offset
-            messages = self.iter_messages(tg_channel, limit=self.INITIAL_MSG_OFFSET)
-        else:
-            messages = self.iter_messages(tg_channel, min_id=message_id)
+        messages = self.get_messages_from_offset(tg_channel, message_id)
 
         max_msg_id = message_id
         async for message in messages:
             max_msg_id = max(max_msg_id, message.id)
             if message.media:
-                if channel['forward']:
-                    await self.forward_messages(
-                        self.TG_BOT_NAME,
-                        message,
-                        tg_channel,
-                        schedule=datetime.timedelta(0, self.MESSAGE_SCHEDULE),
-                    )
-                else:
-                    await self.send_message(
-                        self.TG_BOT_NAME,
-                        message,
-                        schedule=datetime.timedelta(0, self.MESSAGE_SCHEDULE),
-                    )
+                await self.send_message(
+                    entity=self.TG_ADMIN_NAME,
+                    message=message,
+                    schedule=datetime.timedelta(0, self.MESSAGE_SCHEDULE),
+                )
             else:
                 logging.info(f'Message {message.id} from channel {channel["name"]} has no media')
 
@@ -103,3 +97,46 @@ class TGClient(TelegramClient):
             return
 
         self.db.update_channel_offset(tg_channel.channel_id, max_msg_id)
+
+    async def crawl_admin_reactions(self, admin):
+        """
+        Get admin chat and offset
+        Receive all messages from offset to now
+        Iterate over messages and send them to channel (if reactions good)
+        Find max message and update offset
+        """
+        admin_entity = await self.get_input_entity(channel['name'])
+        message_id = self.db.get_admin_offset(admin_entity.id)
+
+        messages = self.get_messages_from_offset(admin_entity, message_id)
+
+        max_msg_id = message_id
+        async for message in messages:
+            if message.reactions:
+                max_msg_id = max(max_msg_id, message.id)
+                reactions = {x.reaction.emoticon: x.count for x in message.reactions.results}
+                if reactions.get('👍', 0) == self.REACTIONS_APPROVED_AMOUNT:
+                    await self.send_message(
+                        entity=self.TG_CHANNEL_NAME,
+                        message=message,
+                        schedule=datetime.timedelta(0, self.MESSAGE_SCHEDULE),
+                    )
+
+        if max_msg_id == message_id:
+            logging.info(f'No new reactions from admin {admin_entity.id}')
+            return
+
+        self.db.update_admin_offset(admin_entity.id, max_msg_id)
+        return
+
+    def get_messages_from_offset(self, entity, offset_message_id):
+        kwargs = {'entity': entity}
+        if offset_message_id == -1:
+            logging.error(f'Entity {entity.id} not initialized, skipping...')
+            return
+        elif offset_message_id == 0:
+            # crawling only last messages - OFFSET for new channels with 0 offset
+            kwargs['limit'] = self.INITIAL_MSG_OFFSET
+        else:
+            kwargs['min_id'] = offset_message_id
+        return self.iter_messages(**kwargs)
