a5b55bf3a1560359771d18e1783d8b262c120b49
Alexander Loginov <56697865+Loganche@users.noreply.github.com>
2024-10-28 22:35:16 +0300

    TG_client refactoring (#10)
    Made universal methods, separated channel/admin specific logic to
    separate methods
    
    ---
    - To see the specific tasks where the Asana app for GitHub is being
    used, see below:
      - https://app.asana.com/0/0/1208598758785271

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import logging
 from asyncio import sleep
 from argparse import ArgumentParser
 from datetime import datetime, timedelta
+from functools import partial
 
 # local modules
 from src.db.db import Sqlite3
@@ -58,13 +59,29 @@ async def main():
         logging.error('Got error from TG Client, exiting...')
         db.connection.close()
         exit(-1)
-    await client.add_new_channels_db()
-    await client.add_new_admins_db()
+    await client.add_new_entities_db(['channel_id', 'message_id'], 'channels', client.channels)
+    await client.add_new_entities_db(['user_id', 'message_id'], 'admins', client.admins)
 
     while not killer.kill_now:
         logging.info(f'Crawling, next run at {datetime.now()+timedelta(seconds=10)}')
-        await client.crawl_channels()
-        await client.crawl_admins()
+        await client.crawl_entities(
+            client.channels,
+            partial(
+                client.crawl_entity_messages,
+                table='channels',
+                where_id='channel_id',
+                message_function=client.send_channel_message,
+            ),
+        )
+        await client.crawl_entities(
+            client.admins,
+            partial(
+                client.crawl_entity_messages,
+                table='admins',
+                where_id='user_id',
+                message_function=client.check_admin_reaction,
+            ),
+        )
         await sleep(10)
 
     db.connection.close()
